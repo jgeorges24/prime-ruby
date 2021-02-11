0e8c73f9ee7d494215a9f0bf9bb5f80022596b60
JEAN-BAPTISTE <jgeorges24@icloud.com>
2021-02-10 19:55:12 -0500

    Done.

--- spec/prime_spec.rb
+++ spec/prime_spec.rb
@@ -1,15 +1,15 @@
-# require_relative './spec_helper'
-# require_relative '../prime.rb'
+ #require_relative './spec_helper'
+ #require_relative '../prime.rb'
 
-# describe "prime?" do
-#   it 'returns true for prime numbers' do
+ describe "prime?" do
+   it 'returns true for prime numbers' do
 #     expect(prime?(2)).to be(true)
 #     expect(prime?(3)).to be(true)
 #     expect(prime?(11)).to be(true)
 #     expect(prime?(105557)).to be(true)
-#   end
+   end
 
-#   it 'returns false for non-prime numbers' do
+   it 'returns false for non-prime numbers' do
 #     expect(prime?(-1)).to be(false), "Be sure to account for negative numbers!"
 #     expect(prime?(0)).to be(false)
 #     expect(prime?(1)).to be(false)
@@ -17,5 +17,5 @@
 #     expect(prime?(40)).to be(false)
 #     expect(prime?(1763)).to be(false)
 #     expect(prime?(101013)).to be(false)
-#   end
-# end
+   end
+ end
